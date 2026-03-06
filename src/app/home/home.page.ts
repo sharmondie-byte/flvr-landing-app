601c2e7c7a2645b8e886f5f679803d079a144e01
Sharmondie Mc Lean <sharmondie@gmail.com>
2026-03-06 14:08:39 +0200

    Initial Ionic landing page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonicModule,        // gives you ion-grid, ion-row, ion-col, ion-card, etc.
+    ReactiveFormsModule // enables [formGroup] and validation
+  ]
 })
 export class HomePage {
-  constructor() {}
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted:', this.contactForm.value);
+      alert('Thank you! We will get back to you.');
+      this.contactForm.reset();
+    }
+  }
 }
+
